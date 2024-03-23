c85136f0ccc87ae89fa8cec8102e7f7c7d72e3ab
Aiden <marcin.madry@interia.com>
2024-03-23 17:35:56 +0100

    Add string buffer

--- code/base/base_string.c
+++ code/base/base_string.c
@@ -1,3 +1,11 @@
+internal usize str8_cstr_size(const char *cstr)
+{
+    u8 *ptr = (u8 *) cstr;
+    for (;*ptr != 0; ++ptr);
+    usize result = (ptr - ((u8 *) cstr));
+    return(result);
+}
+
 internal String8 str8_make(u8 *data, usize size)
 {
     String8 result = {0};
@@ -6,14 +14,19 @@ internal String8 str8_make(u8 *data, usize size)
     return(result);
 }
 
-internal String8 str8_cstr(const char *str)
+internal String8 str8_push_cstr(Arena *arena, const char *cstr)
 {
-    u8 *data = (u8 *) str; 
-    u8 *ptr = data;
-    
-    for (;*ptr != 0; ++ptr);
-    usize size = (ptr - data);
-    
+    String8 result = {0};
+    result.size = str8_cstr_size(cstr);
+    result.data = arena_push_array(arena, u8, result.size + 1);
+    MemoryCopy(result.data, cstr, result.size);
+    return(result);
+}
+
+internal String8 str8_from_cstr(const char *cstr)
+{
+    u8 *data = (u8 *) cstr; 
+    usize size = str8_cstr_size(cstr);
     String8 result = str8_make(data, size);
     return(result);
 }
@@ -24,7 +37,28 @@ internal String8 str8_push_copy(Arena *arena, String8 str)
     result.data = arena_push_array(arena, u8, str.size + 1);
     result.size = str.size;
     MemoryCopy(result.data, str.data, str.size);
-    
     result.data[result.size] = 0;
+    return(result);
+}
+
+internal String8_Buffer str8_buffer_push_empty(Arena *arena, usize cap)
+{
+    String8_Buffer result = {0};
+    result.contents.data = arena_push_array(arena, u8, cap + 1);
+    result.contents.size = 0;
+    result.cap = cap;
+    return(result);
+}
+
+internal b32 str8_buffer_hold(String8_Buffer *buff, String8 str)
+{
+    b32 result = 0;
+    if (str.size <= buff->cap) {
+        MemoryCopy(buff->contents.data, str.data, str.size);
+        buff->contents.size = str.size;
+        buff->contents.data[buff->contents.size] = 0; 
+        result = 1;
+    }
+    
     return(result);
 }
--- code/base/base_string.h
+++ code/base/base_string.h
@@ -6,9 +6,20 @@ typedef struct {
     usize size;
 } String8;
 
+typedef struct {
+    String8 contents;
+    usize cap;
+} String8_Buffer;
+
 internal String8 str8_make(u8 *data, usize size);
-#define str8(cstr) str8_make((u8 *) (cstr), sizeof(cstr) - 1)
-internal String8 str8_cstr(const char *str);
+internal String8 str8_push_cstr(Arena *arena, const char *cstr);
+internal String8 str8_cstr(const char *cstr);
 internal String8 str8_push_copy(Arena *arena, String8 str);
+internal usize str8_cstr_size(const char *cstr);
+
+internal String8_Buffer str8_buffer_push_empty(Arena *arena, usize cap);
+internal b32 str8_buffer_hold(String8_Buffer *buff, String8 str);
+
+#define str8(cstr) str8_make((u8 *) (cstr), sizeof(cstr) - 1)
 
 #endif // BASE_STRING_H
